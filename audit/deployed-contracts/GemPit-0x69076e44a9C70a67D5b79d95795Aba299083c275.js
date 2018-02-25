78ba73f9dd6f2434a9e5d937726a0641a2136545
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2018-02-25 17:20:54 +1100

    Contract setup in JS

--- audit/deployed-contracts/GemPit-0x69076e44a9C70a67D5b79d95795Aba299083c275.js
+++ audit/deployed-contracts/GemPit-0x69076e44a9C70a67D5b79d95795Aba299083c275.js
@@ -1,2 +1,3 @@
-gemPitAddress="0x69076e44a9C70a67D5b79d95795Aba299083c275";
-gemPitAbi=[{"constant":false,"inputs":[{"name":"gem","type":"address"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
+var gemPitAddress="0x69076e44a9C70a67D5b79d95795Aba299083c275";
+var gemPitAbi=[{"constant":false,"inputs":[{"name":"gem","type":"address"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
+var gemPit=web3.contract(gemPitAbi).at(gemPitAddress);
